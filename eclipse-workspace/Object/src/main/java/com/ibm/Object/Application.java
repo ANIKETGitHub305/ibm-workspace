42611fd4ec9ffe64a27e84e5899f60305b0f57db
Aniket <sonusahoo305@gmail.com>
2025-05-05 10:37:55 +0530

    Object

---
+++ eclipse-workspace/Object/src/main/java/com/ibm/Object/Application.java
@@ -0,0 +1,32 @@
+package com.ibm.Object;
+
+
+class Point{
+	public int x,y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+	}
+}
+
+class Test{
+	int var1,var2;
+	public void firstMethod(int var2) {
+		secondMethod();
+	}
+	private void secondMethod() {
+		int var1 = 3;
+		int var2 = 6;
+	}
+}
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+		Point point1 = new Point(2,4);
+		Point point2 = new Point(2,5);
+		
+		System.out.println(point1.equals(point2));
+	}
+}
