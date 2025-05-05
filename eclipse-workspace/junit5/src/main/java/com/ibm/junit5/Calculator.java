4dc92a89111289aeca848ff05fe137efca24245b
Aniket <sonusahoo305@gmail.com>
2025-05-05 15:25:11 +0530

    Added the Boolean , Divided , Sum and Getconnections in JUnit5

--- eclipse-workspace/junit5/src/main/java/com/ibm/junit5/Calculator.java
+++ eclipse-workspace/junit5/src/main/java/com/ibm/junit5/Calculator.java
@@ -5,4 +5,22 @@ public class Calculator {
 	public int sum(int iNum1,int iNum2) {
 		return iNum1+iNum2;
 	}
+	
+	public int divide(int iNum1,int iNum2) {
+		return iNum1/iNum2;
+	}
+	
+	public static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+	
+	public void getConnection() {
+//		try {
+//			Thread.sleep(101);
+//		} catch (InterruptedException e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}
+		System.out.println("Connected...");
+	}
 }
--- eclipse-workspace/junit5/src/test/java/com/ibm/junit5/CalculatorTest.java
+++ eclipse-workspace/junit5/src/test/java/com/ibm/junit5/CalculatorTest.java
@@ -2,11 +2,15 @@ package com.ibm.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
 
@@ -31,12 +35,44 @@ class CalculatorTest {
 	}
 
 	@Test
-	void test() {
+	void testSum() {
 		int except =6;
 		Calculator calculator = new Calculator();
 		int actual = calculator.sum(4, 2);
 		assertEquals(except, actual);
 		//fail("Not yet implemented");
 	}
-
+	@Test
+	void testDivide() {
+		int except =2;
+		Calculator calculator = new Calculator();
+		int actual = calculator.divide(4, 2);
+		assertEquals(except, actual);
+		//fail("Not yet implemented");
+		
+	}
+	@Test
+	void testDivideByZeroException() {
+		System.out.println("divide");
+		Calculator calculator = new Calculator();
+		ArithmeticException exception = assertThrows(ArithmeticException.class,() -> calculator.divide(10, 0),
+				"Expected divide() to throw, but it didn't");
+		assertEquals("/ by zero", exception.getMessage());
+	}
+	@Test
+	void testSlowMethodPerformance() {
+		Calculator calculator = new Calculator();
+		
+		assertTimeout(Duration.ofMillis(100), () -> calculator.getConnection());
+	}
+	@ParameterizedTest
+	@ValueSource(ints = {2,4,6,8,10})
+	void testIsEvenWithEvenNumbers(int number) {
+		assertTrue(Calculator.isEven(number));
+	}
+	@ParameterizedTest
+	@ValueSource(ints = {1,3,5,7,9})
+	void testIsEvenWithOddNumbers(int number) {
+	assertFalse(Calculator.isEven(number));
+	}
 }
