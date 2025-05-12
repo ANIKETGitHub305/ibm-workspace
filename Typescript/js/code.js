ef979d5b775d5e29f047b5055fbd7dabb8240a55
Aniket <sonusahoo305@gmail.com>
2025-05-12 13:33:35 +0530

    Console Example

--- Typescript/js/code.js
+++ Typescript/js/code.js
@@ -1,7 +1,15 @@
-//DOM manipulation example
-var demoElement = document.getElementById("demo");
-if (demoElement) {
-    demoElement.style.fontSize = "60px";
-    demoElement.style.color = "red";
-    demoElement.style.background = "black";
-}
+//Console Example
+console.info('You are awesome');
+console.log('IBM Technology');
+console.log({
+    name: "Aniket Sahoo",
+    age: 23,
+});
+console.log(['apple', 'Banana', 'Orange']);
+console.error('Something is wrong');
+console.log(['Orange', 'Apple', 'Grave']);
+console.assert(2 > 3, "it cannot be");
+console.clear();
+console.warn('I usually get Ignored');
+console.time();
+console.timeEnd();
