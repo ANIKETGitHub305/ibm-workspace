16596ff406ade296f4f6460d0372af3c0d03d2d5
Aniket <sonusahoo305@gmail.com>
2025-05-13 17:04:28 +0530

    Spring Dependancy Injections with Property

--- eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
+++ eclipse-workspace/spring/src/main/java/com/ibm/spring/TextEditor.java
@@ -2,10 +2,22 @@ package com.ibm.spring;
 
 public class TextEditor {
 	private SpellChecker spellChecker;
-	public TextEditor(SpellChecker spellChecker) {
-		System.out.println("Inside Text Box");
+	
+
+	public void setSpellChecker(SpellChecker spellChecker) {
 		this.spellChecker = spellChecker;
 	}
+
+	public TextEditor() {
+		
+	}
+	
+	public TextEditor(SpellChecker spellChecker) {
+		System.out.println("inside TextEDitor construct");
+		this.spellChecker = spellChecker;
+	}
+	
+	
 	
 	public void spellCheck() {
 		spellChecker.checkSpelling();
